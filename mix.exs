87055b0087d400dfbf19ddf9a3ca51427a2f5650
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-03-27 11:40:09 +0200

    Added ueberauth dependency

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule Engage.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:ueberauth, "~> 0.7"}
     ]
   end
 
