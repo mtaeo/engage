2876d30bb3e7684f431d3afce74555748bc4f9f0
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-04-07 23:18:31 +0200

    Implemented homescreen design with placeholder data

--- lib/engage_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule EngageWeb.PageController do
-  use EngageWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
---
+++ lib/engage_web/live/components/top_navigation_component.ex
@@ -0,0 +1,7 @@
+defmodule EngageWeb.TopNavigationComponent do
+  use EngageWeb, :live_component
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+end
