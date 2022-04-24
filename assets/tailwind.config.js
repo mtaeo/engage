0806e84e5166f885a4c5a231b64c641ad323823a
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-04-25 00:04:15 +0200

    Added consistent theme (#37)

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -12,9 +12,34 @@ module.exports = {
 				'pattern-dark': "url('/images/background-dark.svg')",
 				'pattern-light': "url('/images/background-light.svg')"
 			},
+			colors: {
+				accent: {
+					100: '#f9e6ff',
+					200: '#edc3fe',
+					300: '#d88efb',
+					400: '#ba5df4',
+					500: '#9728e6',
+					600: '#6c0fb8',
+					700: '#470684',
+					800: '#2c025a',
+					900: '#190038',
+				},
+				gray: {
+					50:  '#fcfcfd',
+					100: '#dee0e3',
+					200: '#c4c6ca',
+					300: '#a0a5ab',
+					400: '#7c8188',
+					500: '#5d6269',
+					600: '#444850',
+					700: '#33373d',
+					800: '#25272d',
+					900: '#17181c',
+				},
+			},
 			fontFamily: {
 				'sans': ['"Open Sans"', 'sans-serif'],
-				'title': ['"Poppins"', 'sans-serif']
+				'display': ['"Poppins"', 'sans-serif']
 			},
 			gridTemplateColumns: {
 				'presentation': '3fr 2fr',
