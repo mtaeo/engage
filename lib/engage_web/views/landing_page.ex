cd7a53cc30b11d08565cbf42f551f1df646ffcc5
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-04-28 22:52:09 +0200

    Improved code and design of landing page links

--- lib/engage_web/views/landing_page.ex
+++ lib/engage_web/views/landing_page.ex
@@ -2,27 +2,29 @@ defmodule EngageWeb.LandingPageView do
   use EngageWeb, :view
   alias Engage.Users.User
 
-  defp build_based_on_authentication(conn, nil) do
-    content_tag(:div) do
-      [
-        link("Login",
-          to: Routes.user_session_path(conn, :new),
-          class: "underline hover:text-neutral-300 transition-colors"
-        ),
-        " | ",
-        link("Register",
-          to: Routes.user_registration_path(conn, :new),
-          class: "underline hover:text-neutral-300 transition-colors"
-        )
-      ]
-    end
+  defp header_session_links(assigns, nil) do
+    ~H"""
+    <%= link "Login", to: Routes.user_session_path(assigns, :new), class: "px-4 py-1 rounded-xl border-2 text-neutral-200 border-neutral-200 hover:text-neutral-400 hover:border-neutral-400 transition-colors" %>
+    
+    <%= link "Register", to: Routes.user_registration_path(assigns, :new), class: "px-4 py-1 rounded-xl border-2 text-neutral-200 border-neutral-200 hover:text-neutral-400 hover:border-neutral-400 transition-colors" %>
+    """
   end
 
-  defp build_based_on_authentication(conn, %User{} = _current_user) do
-    link("Start Playing",
-      to: Routes.homescreen_path(conn, :index),
-      class:
-        "block px-6 py-3 rounded-xl text-white bg-accent-500 hover:bg-accent-400 transition-colors"
-    )
+  defp header_session_links(assigns, %User{} = _current_user) do
+    ~H"""
+    <%= link "Open App", to: Routes.homescreen_path(assigns, :index), class: "px-4 py-1 rounded-xl border-2 text-neutral-200 border-neutral-200 hover:text-neutral-400 hover:border-neutral-400 transition-colors" %>
+    """
+  end
+  
+  defp entry_button(assigns, nil, text) do
+    ~H"""
+    <%= link text, to: Routes.user_registration_path(assigns, :new), class: "block px-6 py-3 rounded-xl text-white bg-accent-500 hover:bg-accent-400 transition-colors" %>
+    """
+  end
+  
+  defp entry_button(assigns, %User{} = _current_user, text) do
+    ~H"""
+    <%= link text, to: Routes.homescreen_path(assigns, :index), class: "block px-6 py-3 rounded-xl text-white bg-accent-500 hover:bg-accent-400 transition-colors" %>
+    """
   end
 end
