c7b26fc1b7ab885c938ae9012baffef8f423d2a0
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-04-28 21:37:15 +0200

    Implemented links on landing page based on authentication status

--- lib/engage_web/views/landing_page.ex
+++ lib/engage_web/views/landing_page.ex
@@ -1,3 +1,28 @@
 defmodule EngageWeb.LandingPageView do
   use EngageWeb, :view
+  alias Engage.Users.User
+
+  defp build_based_on_authentication(conn, nil) do
+    content_tag(:div) do
+      [
+        link("Login",
+          to: Routes.user_session_path(conn, :new),
+          class: "underline hover:text-neutral-300 transition-colors"
+        ),
+        " | ",
+        link("Register",
+          to: Routes.user_registration_path(conn, :new),
+          class: "underline hover:text-neutral-300 transition-colors"
+        )
+      ]
+    end
+  end
+
+  defp build_based_on_authentication(conn, %User{} = _current_user) do
+    link("Start Playing",
+      to: Routes.homescreen_path(conn, :index),
+      class:
+        "block px-6 py-3 rounded-xl text-white bg-accent-500 hover:bg-accent-400 transition-colors"
+    )
+  end
 end
