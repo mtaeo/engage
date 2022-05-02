991d954a8da134ac441cf57bbe0b90d4ca12867f
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-05-02 15:47:24 +0200

    User settings validation part 1

--- lib/engage/user_settings/profile.ex
+++ lib/engage/user_settings/profile.ex
@@ -1,14 +1,14 @@
 defmodule Engage.UserSettings.Profile do
   import Ecto.Changeset
-  
+
   @types %{email: :string, username: :string, bio: :string, theme: :string}
-  
+
   defstruct [:email, :username, :bio, :theme]
-  
+
   def changeset(%__MODULE__{} = profile, params \\ %{}) do
     {profile, @types}
     |> cast(params, Map.keys(@types))
     |> validate_required([:email, :username, :bio, :theme])
-    |> validate_format(:email, ~R/[a-z0-9.]+@[a-z0-9.]+.[a-z]+/)
+    |> validate_format(:email, ~R/[a-z0-9.]+@[a-z0-9.]+.[a-z]+/, message: "Invalid e-mail format")
   end
 end
