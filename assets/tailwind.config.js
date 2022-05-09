e89262ac02dcaf01bb0ce484b15102ea77ff1bc2
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-05-09 23:33:45 +0200

    Styled alerts (#78)

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -10,24 +10,47 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			animation: {
+				'alert-enter-down': 'alert-enter-down 0.3s ease-out',
+				'alert-leave-up': 'alert-leave-up 5s ease-out'
+			},
+			keyframes: {
+				'alert-enter-down': {
+					'from': {
+						transform: 'translate(-50%, calc(-100% - 8rem))'
+					},
+					'to': {
+						transform: 'translate(-50%, 0)'
+					}
+				},
+				'alert-leave-up': {
+					'0%, 90%': {
+						transform: 'translate(-50%, 0)'
+					},
+					'100%': {
+						transform: 'translate(-50%, calc(-100% - 8rem))'
+					}
+				}
+			},
 			backgroundImage: {
 				'pattern-dark': "url('/images/background-dark.svg')",
 				'pattern-light': "url('/images/background-light.svg')"
 			},
 			boxShadow: {
-				'equal': '0 0 12px -3px hsl(0 0% 0% / 0.5)'
+				'equal': '0 0 12px -3px hsl(0 0% 0% / 0.5)',
+				'equal-lg': '0 0 12px 0px hsl(0 0% 0% / 0.5)'
 			},
 			colors: {
 				accent: {
-					100: '#f9e6ff',
+					100: '#f9e5ff',
 					200: '#edc3fe',
 					300: '#d88efb',
 					400: '#ba5df4',
-					500: '#9728e6',
-					600: '#6c0fb8',
-					700: '#470684',
-					800: '#2c025a',
-					900: '#190038',
+					500: '#9727e7',
+					600: '#6b18af',
+					700: '#471278',
+					800: '#2d0d4f',
+					900: '#1a0830',
 				},
 				gray: {
 					50:  '#fcfcfd',
@@ -53,6 +76,9 @@ module.exports = {
 			spacing: {
 				'120': '30rem',
 				'144': '36rem'
+			},
+			translate: {
+				'alert-hide': 'calc(-100% - 8rem)'
 			}
 		}
 	},
