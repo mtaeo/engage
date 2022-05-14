3b7178ed0ead3338a10b4f317c239a59ef8fb9d0
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-05-15 01:59:23 +0200

    Minor cleanup and added simple database seeding data

--- lib/engage_web/live/game_info_live.ex
+++ lib/engage_web/live/game_info_live.ex
@@ -16,27 +16,4 @@ defmodule EngageWeb.GameInfoLive do
     route = "/games/#{socket.assigns.game.name}/#{game_id}"
     {:noreply, push_redirect(socket, to: route)}
   end
-
-  defp game_list do
-    [
-      %{
-        display_name: "Tic Tac Toe",
-        name: "tic-tac-toe",
-        description: "The classic pen-and-paper game for 2 players.",
-        xp_multiplier: 3,
-        image_path: "/images/tic_tac_toe.jpeg",
-        shadow_color: "shadow-red-600",
-        game_view: EngageWeb.TicTacToeLive
-      },
-      %{
-        display_name: "Memory",
-        name: "memory",
-        description: "",
-        xp_multiplier: 3,
-        image_path: "/images/memory.jpeg",
-        shadow_color: "shadow-cyan-500",
-        game_view: nil
-      }
-    ]
-  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,56 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Engage.Repo.insert!(%Engage.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Engage.Repo
+alias Engage.Games.Game
+alias Engage.Users.User
+
+Repo.insert!(%Game{
+  name: "tic-tac-toe",
+  display_name: "Tic-Tac-Toe",
+  description: "The classic pen-and-paper game for 2 players.",
+  xp_multiplier: 1,
+  type: :multiplayer,
+  image_path: "/images/tic_tac_toe.jpeg",
+  shadow_color: "shadow-red-600"
+})
+
+Repo.insert!(%Game{
+  display_name: "Memory",
+  name: "memory",
+  description: "Unknown description as of right now.",
+  xp_multiplier: 3,
+  type: :multiplayer,
+  image_path: "/images/memory.jpeg",
+  shadow_color: "shadow-cyan-500"
+})
+
+Repo.insert!(
+  User.registration_changeset(
+    %User{},
+    %{
+      username: "test",
+      email: "test@test.com",
+      password: "test"
+    }
+  )
+)
+
+Repo.insert!(
+  User.registration_changeset(
+    %User{},
+    %{
+      username: "moderator",
+      email: "moderator@moderator.com",
+      password: "test"
+    }
+  )
+)
+
+Repo.insert!(
+  User.registration_changeset(
+    %User{},
+    %{
+      username: "admin",
+      email: "admin@admin.com",
+      password: "test"
+    }
+  )
+)
