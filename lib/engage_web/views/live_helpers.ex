b9e62aca9620e894300e155790a68f56f12987a3
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-05-15 13:35:19 +0200

    Added live helpers

---
+++ lib/engage_web/views/live_helpers.ex
@@ -0,0 +1,34 @@
+defmodule EngageWeb.LiveHelpers do
+  alias Phoenix.LiveView
+  alias Phoenix.LiveView.Socket, as: Socket
+  alias Engage.XpToLevels
+  alias EngageWeb.Router.Helpers, as: Routes
+
+  @type session :: map()
+  @type user :: map()
+
+  @spec live_auth_check(Socket.t(), session, (Socket.t(), user -> Socket.t())) :: Socket.t()
+  def live_auth_check(socket, session, success) do
+    case session do
+      %{"current_user" => user} ->
+        success.(socket, user)
+
+      _ ->
+        LiveView.redirect(socket,
+          to: Routes.landing_page_path(socket, :index)
+        )
+    end
+  end
+
+  @spec live_template_assigns(Socket, user) :: Socket
+  def live_template_assigns(socket, user) do
+    level = XpToLevels.calculate_level_for_xp(user.total_xp)
+    upper_xp = XpToLevels.calculate_upper_xp_for_level(level)
+
+    LiveView.assign(socket,
+      coins: user.coins,
+      level: level,
+      level_progress: user.total_xp / (upper_xp || user.total_xp) * 100
+    )
+  end
+end
