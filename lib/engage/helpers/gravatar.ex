f2ac8db1a6f155f90a27707a440b18da4e147e72
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-05-22 18:05:12 +0200

    Changed Gravatar default avatar size

--- lib/engage/helpers/gravatar.ex
+++ lib/engage/helpers/gravatar.ex
@@ -1,6 +1,6 @@
 defmodule Engage.Helpers.Gravatar do
   @base_path "https://www.gravatar.com/avatar/"
-  def get_image_src_by_email(email, style \\ :retro, size \\ 200)
+  def get_image_src_by_email(email, style \\ :retro, size \\ 256)
       when is_binary(email) and is_atom(style) and is_number(size) do
     email =
       email
