848c17d60156e5319ae3f8d2a920850ebee80d91
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-05-26 15:03:05 +0200

    Implemented creation of guest user acc

--- lib/engage_web/controllers/user_oauth_controller.ex
+++ lib/engage_web/controllers/user_oauth_controller.ex
@@ -7,8 +7,28 @@ defmodule EngageWeb.UserOauthController do
   plug Ueberauth
   @rand_pass_length 32
 
-  def request(_conn, _params) do
-    # render(conn, "request.html", callback_url: Helpers.callback_url(conn))
+  def request(conn, _params) do
+    redirect(conn, to: "/auth/guest/callback")
+  end
+
+  def callback(conn, %{"provider" => "guest"}) do
+    username = generate_unique_username("guest")
+
+    user_params = %{
+      username: username,
+      email: username <> "@engage.com",
+      password: random_password(),
+    }
+
+    case Users.fetch_or_create_user(user_params) do
+      {:ok, user} ->
+        UserAuth.log_in_user(conn, user)
+
+      _ ->
+        conn
+        |> put_flash(:error, "Authentication failed")
+        |> redirect(to: "/")
+    end
   end
 
   def callback(%{assigns: %{ueberauth_auth: %{info: user_info}}} = conn, %{"provider" => _}) do
